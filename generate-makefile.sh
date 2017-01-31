59964db53a94ae0e2aa5146f7f9a0d0e1f32e6a1
p.e.visscher <p.e.visscher@zefiros.eu>
2017-01-31 09:52:38 +0100

    Initial commit

---
+++ generate-makefile.sh
@@ -0,0 +1,2 @@
+~/.zpm/zpm/zpm install-package --allow-install --allow-module
+~/.zpm/zpm/zpm gmake --allow-install
---
+++ zpm.lua
@@ -0,0 +1,70 @@
+
+--[[ @cond ___LICENSE___
+-- Copyright (c) 2017 Zefiros Software.
+--
+-- Permission is hereby granted, free of charge, to any person obtaining a copy
+-- of this software and associated documentation files (the "Software"), to deal
+-- in the Software without restriction, including without limitation the rights
+-- to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+-- copies of the Software, and to permit persons to whom the Software is
+-- furnished to do so, subject to the following conditions:
+--
+-- The above copyright notice and this permission notice shall be included in
+-- all copies or substantial portions of the Software.
+--
+-- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+-- IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+-- FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+-- AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+-- LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+-- OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+-- THE SOFTWARE.
+--
+-- @endcond
+--]]
+
+local zefiros = require( "Zefiros-Software/Zefiros-Defaults", "@head" )
+
+workspace "edupack"
+
+    configurations {"Release", "Debug"}
+    platforms { "x86_64", "x86" }
+
+    startproject( "edupack" )
+	location( "edupack/" )
+	objdir "bin/obj/"
+
+	vectorextensions "SSE2"
+    
+    filter "platforms:x86"
+        targetdir "bin/x86/"
+        debugdir "bin/x86/"
+        architecture "x86"
+    
+    filter "platforms:x86_64"
+        targetdir "bin/x86_64/"
+        debugdir "bin/x86_64/"
+        architecture "x86_64"
+
+    filter "*Debug"
+        targetsuffix "d"
+        defines "DEBUG"
+        symbols "On"
+        optimize "Off"
+
+    filter "*Release"        
+        optimize "Speed"
+        defines "NDEBUG"
+
+    filter {}
+
+    project( "edupack" )
+				
+		kind "ConsoleApp"
+		flags "WinMain"
+
+        zpm.uses "Zefiros-Software/BSPLib"
+		
+		includedirs "edupack/include/"
+		
+		files "edupack/**.cpp"
